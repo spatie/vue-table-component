c3a6bf7d8fea0aa363f6c76d80be7dbd72b5fad0
freek <freek@spatie.be>
2017-06-05 00:47:32 +0200

    add sorting

---
+++ src/components/TableColumnHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <th @click="clicked">
+        {{ column.properties.label }}
+    </th>
+</template>
+
+<script>
+    export default {
+        props: ['column'],
+
+        methods: {
+            clicked(event) {
+                if (this.column.isSortable()) {
+                    this.$emit('click', this.column);
+                }
+
+            }
+        }
+    };
+</script>
--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -5,24 +5,39 @@
         <table>
             <thead>
             <tr>
-                <slot></slot>
+                <table-column-header
+                        @click="changeSorting"
+                        v-for="column in columns"
+                        :key="column.properties.for"
+                        :column="column"
+                />
             </tr>
             </thead>
             <tbody>
-                <table-row :key="row.id" v-for="row in displayedRows" :row="row" :columns="columns" />
+            <table-row v-for="row in displayedRows"
+                       :key="row.id"
+                       :row="row"
+                       :columns="columns"
+            />
             </tbody>
         </table>
+
+        <div style="display:none">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
     import { pick } from 'lodash';
+    import TableColumnHeader from './TableColumnHeader';
     import TableRow from './TableRow';
     import Column from '../classes/Column';
     import Row from '../classes/Row';
 
     export default {
-        components: { 
+        components: {
+            TableColumnHeader,
             TableRow,
         },
 
@@ -36,12 +51,33 @@
             filter: '',
             columns: [],
             rows: [],
+            sort: {
+                fieldName: '',
+                order: '',
+            }
         }),
 
         computed: {
             displayedRows() {
-                return this.rows.filter(row => row.passesFilter(this.filter));
-            }
+                return this.sortedRows.filter(row => row.passesFilter(this.filter));
+            },
+
+            sortedRows() {
+                if (this.sortBy === '') {
+                    return this.rows;
+                }
+
+                return this.rows.sort((row1, row2) => {
+                    let value1 = row1.getValue(this.sort.fieldName);
+                    let value2 = row2.getValue(this.sort.fieldName);
+
+                    if (this.sort.order === 'asc') {
+                        return value2.localeCompare(value1);
+                    }
+
+                    return value1.localeCompare(value2);
+                });
+            },
         },
 
         mounted() {
@@ -53,6 +89,22 @@
             this.rows = this.data.map(rowData => new Row(rowData, this.columns));
         },
 
+        created() {
+            this.sort.fieldName = this.sortBy;
+            this.sort.order = this.sortOrder;
+        },
 
+        methods: {
+            changeSorting(column) {
+                if (this.sort.fieldName !== column.properties.for) {
+                    this.sort.fieldName = column.properties.for;
+                    this.sort.order = 'asc';
+                }
+
+                if (this.sort.fieldName === column.properties.for) {
+                    this.sort.order = (this.sort.order === 'desc' ? 'asc' : 'desc');
+                }
+            },
+        },
     };
 </script>
