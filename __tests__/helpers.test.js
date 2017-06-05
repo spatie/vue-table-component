aea8988d45b4352377c25f31115537ff1d4d367b
Freek Van der Herten <freek@spatie.be>
2017-06-05 13:36:19 +0200

    Update helpers.test.js

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -2,7 +2,7 @@ import { stripHtml } from '../src/helpers';
 
 describe('helper functions', () => {
 
-    it('can strip html', async () => {
+    it('can strip html', () => {
 
         const html = "<div class='jumbotron'><b>Hello</b>";
 
