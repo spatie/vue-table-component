551eeefca0cff6cd467e4debe6ab0560f40daa5b
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-06-12 20:21:01 +0200

    Add settings module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import TableComponent from './components/TableComponent.vue';
 import TableColumn from './components/TableColumn.vue';
+import { mergeSettings } from './settings';
 
 export default {
     install(Vue) {
         Vue.component('table-component', TableComponent);
         Vue.component('table-column', TableColumn);
+    },
+
+    settings(settings) {
+        mergeSettings(settings);
     },
 };
 
