d57a097ec3276b7be267dc0d6f79ab94191a6249
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-08-02 14:36:36 +0200

    Run linter

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -9,50 +9,50 @@
 </template>
 
 <script>
-    import { range } from 'lodash';
+import { range } from 'lodash';
 
-    export default {
-        props: {
-            pagination: {
-                type: Object,
-                default: () => ({}),
-            },
+export default {
+    props: {
+        pagination: {
+            type: Object,
+            default: () => ({}),
+        },
+    },
+
+    computed: {
+        pages() {
+            return this.pagination.total_pages === undefined
+                ? []
+                : range(1, this.pagination.total_pages + 1);
         },
 
-        computed: {
-            pages() {
-                return this.pagination.total_pages === undefined
-                    ? []
-                    : range(1, this.pagination.total_pages + 1);
-            },
+        shouldShowPagination() {
+            if (this.pagination.total_pages === undefined) {
+                return false;
+            }
 
-            shouldShowPagination() {
-                if (this.pagination.total_pages === undefined) {
-                    return false;
-                }
+            if (this.pagination.count === 0) {
+                return false;
+            }
 
-                if (this.pagination.count === 0) {
-                    return false;
-                }
+            return this.pagination.total_pages > 1;
+        },
+    },
 
-                return this.pagination.total_pages > 1;
-            },
+    methods: {
+        isActive(page) {
+            const currentPage = this.pagination.current_page || 1;
+
+            return currentPage === page;
         },
 
-        methods: {
-            isActive(page) {
-                const currentPage = this.pagination.current_page || 1;
+        pageClicked(page) {
+            if (this.pagination.current_page === page) {
+                return;
+            }
 
-                return currentPage === page;
-            },
-
-            pageClicked(page) {
-                if (this.pagination.current_page === page) {
-                    return;
-                }
-
-                this.$emit('choosePage', page);
-            },
+            this.$emit('choosePage', page);
         },
-    };
+    },
+};
 </script>
