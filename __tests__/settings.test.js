91a0c8e0b246349f416747de1170b7e5cce0997e
Brian Faust <hello@brianfaust.me>
2017-09-22 08:00:46 +0300

    add tbody class to settings test

--- __tests__/settings.test.js
+++ __tests__/settings.test.js
@@ -5,11 +5,13 @@ describe('settings', () => {
     it('can update settings', () => {
         TableComponent.settings({
             tableClass: 'table',
-            theadClass: 'table-head'
+            theadClass: 'table-head',
+            tbodyClass: 'table-body',
         });
 
         expect(settings.tableClass).toBe('table');
         expect(settings.theadClass).toBe('table-head');
+        expect(settings.tbodyClass).toBe('table-body');
         expect(settings.filterPlaceholder).toBe('Filter table…');
         expect(settings.filterNoResults).toBe('There are no matching rows');
     });
