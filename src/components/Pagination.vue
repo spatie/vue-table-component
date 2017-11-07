a36a560c616b2232cce35dd4f591aa70d47e30d6
Alex Ankit <alex.ankit@ithands.net>
2017-11-07 14:31:28 +0530

    Fix pagination links for huge data

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <nav v-if="shouldShowPagination">
         <ul class="pagination justify-content-center">
-            <li class="page-item" :class="{ active: isActive(page) }" v-for="page in pages">
+            <li class="page-item" :class="{ active: isActive(page), disabled: page === '...' }" v-for="page in pages">
                 <a class="page-link" @click="pageClicked(page)">{{ page }}</a>
             </li>
         </ul>
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-    import { range } from '../helpers';
 
     export default {
         props: {
@@ -21,9 +20,53 @@
 
         computed: {
             pages() {
-                return this.pagination.totalPages === undefined
-                    ? []
-                    : range(1, this.pagination.totalPages + 1);
+                if (this.pagination.totalPages === undefined) {
+                    return [];
+                }
+
+                const pages = [];
+                let preDots = false;
+                let postDots = false;
+
+                for (let i = 1; i <= this.pagination.totalPages; i++) {
+                    if (this.pagination.totalPages <= 10) {
+                        pages.push(i);
+                    } else {
+                        if (i === 1) {
+                            pages.push(i);
+                        } else if (i === this.pagination.totalPages) {
+                            pages.push(i);
+                        } else if (
+                            // link is within 4 of current
+                        (i > this.pagination.currentPage - 4 && i < this.pagination.currentPage + 4) ||
+                        // current and link less than 4
+                        (i < 4 && this.pagination.currentPage < 4) ||
+                        // current and link within 4 of end
+                        (i > this.pagination.totalPages - 4 && this.pagination.currentPage > this.pagination.totalPages - 4)) {
+                            pages.push(i);
+                        } else if (i < this.pagination.currentPage && !preDots) {
+                            pages.push('...');
+                            preDots = true;
+                        } else if (i > this.pagination.currentPage && !postDots) {
+                            pages.push('...');
+                            postDots = true;
+                        }
+                    }
+                }
+
+                return pages;
+            },
+
+            shouldShowPagination() {
+                if (this.pagination.totalPages === undefined) {
+                    return false;
+                }
+
+                if (this.pagination.count === 0) {
+                    return false;
+                }
+
+                return this.pagination.totalPages > 1;
             },
 
             shouldShowPagination() {
@@ -47,7 +90,10 @@
             },
 
             pageClicked(page) {
-                if (this.pagination.currentPage === page) {
+                if (page === '...' ||
+                    page === this.pagination.currentPage ||
+                    page > this.pagination.totalPages ||
+                    page < 1) {
                     return;
                 }
 
