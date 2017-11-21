0c015913eabf75a9d660d4f4a7a8022445f91c1b
Alex Ankit <alex.ankit@ithands.net>
2017-11-21 18:31:42 +0530

    Dedicated function to generate pagination links

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -34,41 +34,9 @@
 
         computed: {
             pages() {
-                if (this.pagination.totalPages === undefined) {
-                    return [];
-                }
-
-                const pages = [];
-                let preDots = false;
-                let postDots = false;
-
-                for (let i = 1; i <= this.pagination.totalPages; i++) {
-                    if (this.pagination.totalPages <= 10) {
-                        pages.push(i);
-                    } else {
-                        if (i === 1) {
-                            pages.push(i);
-                        } else if (i === this.pagination.totalPages) {
-                            pages.push(i);
-                        } else if (
-                            // link is within 4 of current
-                        (i > this.pagination.currentPage - 4 && i < this.pagination.currentPage + 4) ||
-                        // current and link less than 4
-                        (i < 4 && this.pagination.currentPage < 4) ||
-                        // current and link within 4 of end
-                        (i > this.pagination.totalPages - 4 && this.pagination.currentPage > this.pagination.totalPages - 4)) {
-                            pages.push(i);
-                        } else if (i < this.pagination.currentPage && !preDots) {
-                            pages.push('...');
-                            preDots = true;
-                        } else if (i > this.pagination.currentPage && !postDots) {
-                            pages.push('...');
-                            postDots = true;
-                        }
-                    }
-                }
-
-                return pages;
+                return this.pagination.totalPages === undefined
+                    ? []
+                    : this.pageLinks();
             },
 
             shouldShowPagination() {
@@ -112,6 +80,53 @@
                 }
 
                 this.$emit('pageChange', page);
+            },
+
+            pageLinks() {
+                const pages = [];
+                let preDots = false;
+                let postDots = false;
+
+                for (let i = 1; i <= this.pagination.totalPages; i++) {
+
+                    if (this.pagination.totalPages <= 10) {
+                        pages.push(i);
+                    } else {
+                        if (i === 1) {
+                            pages.push(i);
+                            continue;
+                        }
+
+                        if (i === this.pagination.totalPages) {
+                            pages.push(i);
+                            continue;
+                        }
+
+                        if (
+                            // link is within 4 of current
+                        (i > this.pagination.currentPage - 4 && i < this.pagination.currentPage + 4) ||
+                        // current and link less than 4
+                        (i < 4 && this.pagination.currentPage < 4) ||
+                        // current and link within 4 of end
+                        (i > this.pagination.totalPages - 4 && this.pagination.currentPage > this.pagination.totalPages - 4)) {
+                            pages.push(i);
+                            continue;
+                        }
+
+                        if (i < this.pagination.currentPage && !preDots) {
+                            pages.push('...');
+                            preDots = true;
+                            continue;
+                        }
+
+                        if (i > this.pagination.currentPage && !postDots) {
+                            pages.push('...');
+                            postDots = true;
+                        }
+                    }
+                }
+
+                return pages;
             },
         },
     };
